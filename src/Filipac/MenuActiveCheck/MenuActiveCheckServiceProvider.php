ce9cfbf59d74fa9761f141ce210c349f49750f8f
Filip Iulian Pacurar <hello@filipac.net>
2015-02-24 11:14:54 +0200

    Allow check for both controler and action
    ```
    @checkActiveBoth Controller&action|active
    ```

--- src/Filipac/MenuActiveCheck/MenuActiveCheckServiceProvider.php
+++ src/Filipac/MenuActiveCheck/MenuActiveCheckServiceProvider.php
@@ -1,53 +1,67 @@
 <?php namespace Filipac\MenuActiveCheck;
 
-use Illuminate\Support\ServiceProvider, Blade;
+use Blade;
+use Illuminate\Support\ServiceProvider;
 
-class MenuActiveCheckServiceProvider extends ServiceProvider {
-
-  /**
-   * Indicates if loading of the provider is deferred.
-   *
-   * @var bool
-   */
-  protected $defer = false;
-
-  /**
-   * Register the service provider.
-   *
-   * @return void
-   */
-  public function register()
+class MenuActiveCheckServiceProvider extends ServiceProvider
 {
-    /*
-     * Register the service provider for the dependency.
-     */
-    $this->app->register('HieuLe\Active\ActiveServiceProvider');
-    /*
-     * Create aliases for the dependency.
-     */
-    $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-    $loader->alias('Active', 'HieuLe\Active\Facades\Active');
-}
 
-  public function boot() {
-    Blade::extend(function($view, $compiler)
-    {
-        return preg_replace('/(\s*){@checkActive(\s*)([a-zA-Z]*?)\|(.*)}/i', '$1<?php $__check = explode(\'\\\\\\\',\HieuLe\Active\Facades\Active::getController()); if(\'$3\' == end($__check)) echo \'$4\'; ?>', $view);
-    });
-    Blade::extend(function($view, $compiler)
-    {
-        return preg_replace('/(\s*){@checkActiveAction(\s*)([a-zA-Z]*?)\|(.*)}/i', '$1<?php $__check = explode(\'\\\\\\\',\HieuLe\Active\Facades\Active::getMethod()); if(\'$3\' == end($__check)) echo \'$4\'; ?>', $view);
-    });
-  }
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-  /**
-   * Get the services provided by the provider.
-   *
-   * @return array
-   */
-  public function provides()
-  {
-    return array();
-  }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        /*
+         * Register the service provider for the dependency.
+         */
+        $this->app->register('HieuLe\Active\ActiveServiceProvider');
+        /*
+         * Create aliases for the dependency.
+         */
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('Active', 'HieuLe\Active\Facades\Active');
+    }
+
+
+    public function boot()
+    {
+        Blade::extend(function ($view, $compiler) {
+            return preg_replace('/(\s*){@checkActive(\s*)([a-zA-Z]*?)\|(.*)}/i',
+                '$1<?php $__check = explode(\'\\\\\\\',\HieuLe\Active\Facades\Active::getController()); if(\'$3\' == end($__check)) echo \'$4\'; ?>',
+                $view);
+        });
+        Blade::extend(function ($view, $compiler) {
+            return preg_replace('/(\s*){@checkActiveAction(\s*)([a-zA-Z]*?)\|(.*)}/i',
+                '$1<?php $__check = explode(\'\\\\\\\',\HieuLe\Active\Facades\Active::getMethod()); if(\'$3\' == end($__check)) echo \'$4\'; ?>',
+                $view);
+        });
+        Blade::extend(function ($view, $compiler) {
+            return preg_replace('/(\s*){@checkActiveBoth(\s*)([a-zA-Z]*?)&([a-zA-Z]*?)\|(.*)}/i',
+                '$1<?php $__check = explode(\'\\\\\\\',\HieuLe\Active\Facades\Active::getMethod());
+                $__check2 = explode(\'\\\\\\\',\HieuLe\Active\Facades\Active::getController()); if(\'$3\' == end($__check2) AND \'$4\' == end($__check)) echo \'$5\'; ?>',
+                $view);
+        });
+    }
+
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
